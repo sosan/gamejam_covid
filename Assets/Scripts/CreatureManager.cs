1570dace8fe229c9fc8bed1a7626f05b23f66a2c
jose <josloe@gmail.com>
2020-04-04 03:01:55 +0200

    tamagochu

--- Assets/Scripts/CreatureManager.cs
+++ Assets/Scripts/CreatureManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class CreatureManager : MonoBehaviour
 {
@@ -9,14 +10,24 @@ public class CreatureManager : MonoBehaviour
     [SerializeField] private Image indicadorComida = null;
     [SerializeField] private Image indicadorFelicidad = null;
     [SerializeField] private Image indicadorAburrimiento = null;
+	[SerializeField] private TextMeshProUGUI textoNivel = null;
+	[SerializeField] private TextMeshProUGUI textoCantidadMonedas = null;
+	[SerializeField] private TextMeshProUGUI mensajeBocadillo = null;
+	[SerializeField] private GameObject bocadillo = null;
+
+	[SerializeField] private ParticleSystem[] particulasTomogochi = null;
+
+	[SerializeField] public int monedas = 10000;
 
     private float felicidad = 100f;
-	private int nivel = 0;
+	private int nivel = 1;
+	private int progresoNivel = 0;
 	private int cacaContador = 0;
-	private float muerte = 100f;
+	private float muerteContador = 100f;
 	private float ambriento = 0f;
 	private float cacaPresion = 0f;
 	private float aburrimiento = 0f;
+	
 
 	public bool isSick = false;
 	public bool isDead = false;
@@ -26,15 +37,245 @@ public class CreatureManager : MonoBehaviour
 	public bool isMinigameRunning = false;
 
 
-    // Start is called before the first frame update
+    //System.Diagnostics.Stopwatch timer;
+
+
     void Start()
     {
+
+		mensajeBocadillo.text = "";
+		bocadillo.SetActive(false);
+		textoNivel.text = nivel.ToString("N0");
+		ActualizarMonedas();
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isMinigameRunning == true) return;
+
+        if (isDead)
+        {
+			return;
+		}
         
+
+        ambriento += 0.05f; //0.003f;
+		cacaPresion += 0.02f;
+
+
+		if (isBored) 
+		{
+			felicidad -= 0.010f; //0.005f;
+		} 
+		else
+		{ 
+			aburrimiento += 0.010f;  //0.002f;
+		
+		}
+		
+
+		if (isSick) 
+		{
+			felicidad -= 0.010f;// 0.005f;
+			muerteContador -= 0.010f; //0.05f;
+		}
+
+		if (isHungry) 
+		{
+			felicidad -= 0.10f; //0.05f;
+		}
+
+		if (ambriento > 70f && isHungry == false) 
+		{
+			
+			isHungry = true;
+			//falta animacion del tamagochi
+			mensajeBocadillo.text = "Tengo<br>Hambre!";
+			bocadillo.SetActive(true);
+
+			
+		}
+
+		if (aburrimiento > 80f && isBored == false) 
+		{
+			SetAburrido ();
+		}
+
+		if (cacaPresion >= 100f) 
+		{
+			SetSucioCaca();
+			cacaPresion = Random.Range (0, 20f);
+		}
+
+
+		if (!isAsleep && (System.DateTime.Now.Hour > 23 || System.DateTime.Now.Hour < 7)) 
+		{
+			SetDormido ();
+		} 
+		else if (isAsleep && (System.DateTime.Now.Hour > 7 && System.DateTime.Now.Hour < 23))
+		{
+			SetDespierto ();
+		}
+			
+		if (felicidad <= 0 || muerteContador <= 0 || ambriento >= 100) 
+		{
+			SetMuerto();
+		}
+
+		indicadorComida.fillAmount = ambriento/100f;
+		indicadorAburrimiento.fillAmount = aburrimiento/100f;
+		indicadorFelicidad.fillAmount = felicidad/100f;
+
+
+
+
+
+
     }
+
+
+	private void SetMuerto()
+	{ 
+		if (nivel > 0)
+		{
+			//falta animacion de tamagochi
+			//falta sonido muerto
+			
+			// falta imagen muerto
+
+			isDead = true;
+		}
+	
+	}
+
+
+	private void SetDespierto()
+	{ 
+	
+		//falta imagen despierto
+	}
+
+	private void SetDormido()
+	{ 
+	
+		//falta imagen dormido
+	}
+
+	private void SetSucioCaca()
+	{ 
+	
+	
+	}
+
+
+	private void SetAburrido()
+	{ 
+	
+	
+	}
+
+
+ //   public void startTimer ()
+	//{
+	//	timer.Start ();
+	//}
+
+	private void ActualizarNivel()
+	{ 
+		if (nivel < 10)
+		{ 
+		
+			textoNivel.text = "NIVEL 0" + nivel.ToString("N0");
+		}
+		else
+		{ 
+			textoNivel.text = "NIVEL " + nivel.ToString("N0");
+		
+		}
+		
+		
+	
+	}
+
+	private void ActualizarMonedas()
+	{ 
+	
+		monedas--;
+		if (monedas <= 0)
+		{ 
+			monedas = 0;
+		}
+
+
+		if (monedas < 10)
+		{ 
+		
+			textoCantidadMonedas.text = "x0" + monedas.ToString("N0");
+		}
+		else
+		{ 
+			textoCantidadMonedas.text = "x" + monedas.ToString("N0");
+		
+		}
+	
+	}
+
+    public void Click_BotonComida()
+    { 
+		if (monedas <= 0) return;
+		if (ambriento <= 0) return;
+
+		particulasTomogochi[0].Play();
+		ambriento--;
+		progresoNivel++;
+		if (progresoNivel == 20)
+		{ 
+			nivel++;
+		
+		}
+        ActualizarMonedas();
+		ActualizarNivel();
+
+
+    }
+
+    public void Click_BotonFelicidad()
+    { 
+
+		if (monedas <= 0) return;
+		if (felicidad >= 100) return;
+
+		particulasTomogochi[1].Play();
+		felicidad++;
+		ActualizarMonedas();
+    
+    }
+
+    public void Click_BotonArena()
+    { 
+		if (monedas <= 0) return;
+		if (cacaPresion <= 0) return;
+
+		particulasTomogochi[2].Play();
+		cacaPresion--;
+
+		ActualizarMonedas();
+    
+    }
+
+    public void Click_BotonJuego()
+    { 
+		if (monedas <= 0) return;
+		if (aburrimiento <= 0) return;
+		
+
+		particulasTomogochi[3].Play();
+		aburrimiento--;
+		ActualizarMonedas();
+    }
+
+
 }
---
+++ Assets/Snake/Scrips/GameManager.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA
+{
+    public class GameManager : MonoBehaviour
+    {
+        public int maxHeight = 15;
+        public int maxWidth = 17;
+
+        public Color color1;
+        public Color color2;
+        public Color playerColor = Color.black;  
+
+        GameObject mapObject;
+        GameObject PlayerObj;
+
+        SpriteRenderer mapRenderer;
+
+        private Node node;
+
+        Node[,] grid;
+
+
+        private void Start()
+        {
+            CreateMap();
+            PlacePlayer();
+        }
+
+        void CreateMap()
+        {
+            mapObject = new GameObject("map");
+            mapRenderer = mapObject.AddComponent<SpriteRenderer>();
+
+            grid = new Node[maxWidth, maxHeight];
+
+            Texture2D txt = new Texture2D(maxWidth, maxHeight);
+            
+            for (int x = 0; x < maxWidth; x++)
+            {
+                for (int y = 0; y < maxHeight; y++)
+                {
+                    Vector3 tp = Vector3.zero;
+                    tp.x = x;
+                    tp.y = y;
+
+                    Node n = new Node()
+                    {
+                        x = x,
+                        y = y,
+                        worldPosition = tp
+                    };
+                grid[x, y] = n; 
+
+                    #region Visual
+                    if (x % 2 != 0)
+                    {
+                        if (y % 2 != 0)
+                        {
+                            txt.SetPixel(x, y, color1);
+                        }
+                        else
+                        {
+                            txt.SetPixel(x, y, color2);
+                        }
+                    }
+                    else
+                    {
+                        if (y % 2 != 0)
+                        {
+                            txt.SetPixel(x, y, color2);
+                        }
+                        else
+                        {
+                            txt.SetPixel(x, y, color1);
+                        }
+                    }
+                     #endregion
+                }
+            }
+
+            txt.filterMode = FilterMode.Point;
+            
+            txt.Apply();
+            Rect rect = new Rect(0, 0, maxWidth, maxHeight);
+            Sprite sprite = Sprite.Create(txt, rect, Vector2.one * 0.5f, 1, 0, SpriteMeshType.FullRect);
+            mapRenderer.sprite = sprite;
+
+        }
+
+        void PlacePlayer() 
+        {
+            PlayerObj = new GameObject("Player");
+            SpriteRenderer playerRender = PlayerObj.AddComponent<SpriteRenderer>();
+            playerRender.sprite = CreateSprite(playerColor);
+            playerRender.sortingOrder = 1;
+
+            PlayerObj.transform.position = GetNode(3, 3).worldPosition;
+
+        }
+
+        Node GetNode(int x, int y) 
+        {
+            if (x < 0 || x > maxWidth-1 || y < 0 || y > maxHeight-1)
+                return null;
+            return grid[x, y];
+        }
+
+        Sprite CreateSprite(Color targetColor)
+        {
+            Texture2D txt = new Texture2D(1, 1);
+            txt.SetPixel(0, 0, targetColor);
+            txt.Apply();
+            txt.filterMode = FilterMode.Point;
+            Rect rect = new Rect(0, 0, 1, 1);
+            return Sprite.Create(txt, rect, Vector2.one * 0.5f, 1, 0, SpriteMeshType.FullRect);
+        }
+    }
+}
